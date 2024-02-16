4bef99c0e164bde415b230e790e66ec620d120f6
Paul Donovan <paul.m.donovan@outlook.com>
2024-02-17 08:22:58 +1100

    addsSolutionsJSLab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+const mondayWork = function (task = "go to the office") {
+    return "This Monday, I will " + task + ".";
+}
+function wrapAdjective(flair = "*") {
+
+    return function (quality = "special") {
+        return `You are ${flair}${quality}${flair}!`;
+
+    };
+}
+console.log(wrapAdjective("@@@")("amazing programmer"))
